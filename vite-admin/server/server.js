52d68597ab601f9b951e0c32b7a176ebf463a44b
maxxmadhab <madhabpadhi74@gmail.com>
2025-07-21 05:31:40 +0530

    render2.0

--- vite-admin/server/server.js
+++ vite-admin/server/server.js
@@ -24,6 +24,10 @@ app.get("/api", (req, res) => {
 app.use("/api/applications", applicationsRoute);
 
 // Start server
-app.listen(5000, () => {
-  console.log("Server started on port 5000");
+
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
